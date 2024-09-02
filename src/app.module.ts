53eba2af339e7fb613dcaf91940f35b8961b36e5
DaemonDxx <i79535999238@gmail.com>
2024-09-02 17:47:01 +0300

    [feat] add logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configSchema } from '../config/config.schema';
+import { utilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import * as process from 'node:process';
 
 @Module({
   imports: [
@@ -10,7 +13,24 @@ import { configSchema } from '../config/config.schema';
       isGlobal: true,
       envFilePath: ['./config/.evn', './config/.development.env'],
       validationSchema: configSchema,
-    })
+    }),
+    WinstonModule.forRoot({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            utilities.format.nestLike('LKS', {
+              colors: true,
+              prettyPrint: true,
+              processId: true,
+              appName: true,
+            }),
+          ),
+        }),
+      ],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { LoggerService } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  const logger = app.get<LoggerService>(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(logger);
   await app.listen(3000);
 }
+
 bootstrap();
