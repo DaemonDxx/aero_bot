1c7030bf6ff57c804a093c2954fc24713b21e2a1
DaemonDxx <i79535999238@gmail.com>
2024-09-09 13:08:55 +0300

    [feat] add reply service

---
+++ src/telegram/reply.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectBot } from 'nestjs-telegraf';
+import { Message } from 'telegraf/types';
+import { Context, Telegraf } from 'telegraf';
+import { Template } from './templates/template';
+import { ServiceError } from '../utils/errors/service.error';
+
+@Injectable()
+export class ReplyService {
+  constructor(
+    @InjectBot()
+    private readonly bot: Telegraf,
+  ) {}
+
+  async reply(ctx: Context, template: Template): Promise<Message.TextMessage> {
+    try {
+      return await ctx.reply(template.getMessageText(), template.getMarkup());
+    } catch (e) {
+      throw new ServiceError(
+        ReplyService.name,
+        `send message error: ${e.message}`,
+        e,
+      );
+    }
+  }
+
+  async editMessage(
+    msg: Message.TextMessage,
+    template: Template,
+  ): Promise<void> {
+    try {
+      await this.bot.telegram.editMessageText(
+        msg.chat.id,
+        msg.message_id,
+        '',
+        template.getMessageText(),
+        template.getMarkup(),
+      );
+    } catch (e) {
+      throw new ServiceError(
+        ReplyService.name,
+        `edit message error: ${e.message}`,
+        e,
+      );
+    }
+  }
+}
--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -3,19 +3,20 @@ import { TelegrafModule } from 'nestjs-telegraf';
 import { ConfigService } from '@nestjs/config';
 import { session } from 'telegraf';
 import { scenesProviders } from './scenes/scenes.providers';
+import { ReplyService } from './reply.service';
 
 @Module({
   imports: [
     TelegrafModule.forRootAsync({
       useFactory: async (config: ConfigService) => {
         return {
-          token: config.get<string>("TG_BOT_TOKEN"),
+          token: config.get<string>('TG_BOT_TOKEN'),
           middlewares: [session()],
-        }
+        };
       },
       inject: [ConfigService],
     })
   ],
-  providers: [...scenesProviders]
+  providers: [...scenesProviders, ReplyService],
 })
 export class TelegramModule {}
