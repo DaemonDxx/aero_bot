bec0a5cdc464e4af5f5995daff142c734f35f861
DaemonDxx <i79535999238@gmail.com>
2024-09-10 13:20:02 +0300

    [chore] update config validation schema

--- config/config.schema.ts
+++ config/config.schema.ts
@@ -2,5 +2,10 @@ import * as Joi from 'joi';
 
 export const configSchema = Joi.object({
   TG_BOT_TOKEN: Joi.string().required(),
-  GRPC_SERVER_URL: Joi.string().required()
-})
+  GRPC_SERVER_URL: Joi.string().required(),
+  BOT_PG_SESSION_HOST: Joi.string().required(),
+  BOT_PG_SESSION_PORT: Joi.number().port().required(),
+  BOT_PG_SESSION_USER: Joi.string().required(),
+  BOT_PG_SESSION_PASSWORD: Joi.string().required(),
+  BOT_PG_SESSION_DB: Joi.string().required(),
+});
